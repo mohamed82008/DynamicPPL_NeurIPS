74c1396a5181e67b127c76d49f34b59566f2a95d
Martin Trapp <martin.trapp@student.tugraz.at>
2020-06-03 10:34:28 +0200

    updated most results, some still running.

--- submit.sh
+++ submit.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 sbatch --export=ALL,JULIA_DEPOT_PATH='/clusterFS/home/user/martint/julia_depo_2' slurm_turing.sbatch
-sbatch --export=ALL,JULIA_DEPOT_PATH='/clusterFS/home/user/martint/julia_depo_2' slurm_stan.sbatch
-sbatch --export=ALL,JULIA_DEPOT_PATH='/clusterFS/home/user/martint/julia_depo_2' slurm_dynamic.sbatch
+#sbatch --export=ALL,JULIA_DEPOT_PATH='/clusterFS/home/user/martint/julia_depo_2' slurm_stan.sbatch
+#sbatch --export=ALL,JULIA_DEPOT_PATH='/clusterFS/home/user/martint/julia_depo_2' slurm_dynamic.sbatch
